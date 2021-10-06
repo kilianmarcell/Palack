0212bbcd7b23c3449dbc84d9e12497370d7126e4
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-10-06 16:29:23 +0200

    rekesz.txt beolvasva

--- src/hu/petrik/Teszt.java
+++ src/hu/petrik/Teszt.java
@@ -1,16 +1,49 @@
 package hu.petrik;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Teszt {
+    private ArrayList<Rekesz> rekeszek;
 
     public Teszt() {
+        this.rekeszek = new ArrayList<>();
         elso();
+        masodik();
     }
 
     public void masodik() {
         Beolvas();
     }
 
-    public void Beolvas() {}
+    public void Beolvas() {
+        try {
+            BufferedReader bReader = new BufferedReader(new FileReader("palackok.txt"));
+            rekeszek.add(new Rekesz(5000));
+            String sor = bReader.readLine();
+            while (sor != null) {
+                Palack p;
+                String[] egySor = sor.split(";");
+                if (egySor.length == 2){
+                    p = new Palack(egySor[0], Integer.parseInt(egySor[1]));
+                } else if (egySor.length == 3) {
+                    p = new Palack(egySor[0], Integer.parseInt(egySor[1]), Integer.parseInt(egySor[2]));
+                } else {
+                    p = new VisszavalthatoPalack(egySor[0], Integer.parseInt(egySor[1]), Integer.parseInt(egySor[2]), Integer.parseInt(egySor[3]));
+                }
+                boolean siker = rekeszek.get(rekeszek.size()-1).ujPalack(p);
+                if (!siker) {
+                    rekeszek.add(new Rekesz(5000));
+                    rekeszek.get(rekeszek.size()-1).ujPalack(p);
+                }
+                sor = bReader.readLine();
+            }
+        }   catch (IOException e) {
+            e.getMessage();
+        }
+    }
 
     public static void elso() {
         Palack p1 = new Palack("tej", 1000, 1000);
